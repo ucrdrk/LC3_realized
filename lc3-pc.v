2ec48fb7c0c3f3834183cd3cb8cc794b6727e5df
Allan Knight <allan@cs.ucr.edu>
2023-03-29 11:08:14 -0800

    Almost done

---
+++ lc3-pc.v
@@ -0,0 +1,18 @@
+module pc (
+    input wire[15:0] addr_in,
+    input wire clk,
+    input wire en,
+    output wire [15:0] addr_out
+);
+
+reg [15:0] pc;
+
+initial pc = 16'h0000;
+
+always @ (posedge clk) begin
+    if(en === 1) pc = addr_in;
+end
+
+assign addr_out = pc;
+
+endmodule
