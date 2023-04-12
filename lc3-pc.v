e09f673dec23853297fdbedce9f1bf08f099bd3c
Allan Knight <allan@cs.ucr.edu>
2023-04-12 10:17:04 -0700

    Closer to working

--- lc3-pc.v
+++ lc3-pc.v
@@ -2,7 +2,7 @@ module pc (
     input wire[15:0] addr_in,
     input wire clk,
     input wire en,
-    output wire [15:0] addr_out
+    output reg [15:0] addr_out
 );
 
 reg [15:0] pc;
@@ -11,8 +11,7 @@ initial pc = 16'h0200;
 
 always @ (posedge clk) begin
     if(en === 1) pc = addr_in;
+    addr_out <= pc;
 end
-
-assign addr_out = pc;
 
 endmodule
