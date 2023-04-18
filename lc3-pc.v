b306d465229ac672a116d8e69f783490715157c2
Allan Knight <allan@cs.ucr.edu>
2023-04-17 23:05:48 -0700

    Looking good

--- lc3-pc.v
+++ lc3-pc.v
@@ -1,17 +1,17 @@
 module pc (
-    input wire[15:0] a_in,
-    input wire clk,
+    input wire[15:0] D,
+    input wire C,
     input wire en,
-    output reg [15:0] a_out
+    output reg [15:0] Q
 );
 
 reg [15:0] pc;
 
 initial pc = 16'h0200;
 
-always @ (posedge clk) begin
-    if(en === 1) pc = a_in;
-    a_out <= pc;
+always @ (posedge C) begin
+    if(en === 1) pc = D;
+    Q <= pc;
 end
 
 endmodule
